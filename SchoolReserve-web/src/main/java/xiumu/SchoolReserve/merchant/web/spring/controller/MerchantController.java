335b68436e794c58452b41c8997165f5093a72e2
SiuMu <37199920+SiuMu@users.noreply.github.com>
2018-06-27 15:31:05 +0800

    。。

--- SchoolReserve-web/src/main/java/xiumu/SchoolReserve/merchant/web/spring/controller/MerchantController.java
+++ SchoolReserve-web/src/main/java/xiumu/SchoolReserve/merchant/web/spring/controller/MerchantController.java
@@ -1,4 +1,4 @@
-package xiumu.SchoolReserve.Merchant.web.spring.controller;
+package xiumu.SchoolReserve.merchant.web.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
